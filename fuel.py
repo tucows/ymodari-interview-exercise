4c6820a72a494f4164cb2dcf25f2bfa8f586788b
Yasmin Modarai <67163194+fluff905@users.noreply.github.com>
2021-11-09 06:28:02 -0500

    Update fuel.py

--- fuel.py
+++ fuel.py
@@ -5,7 +5,7 @@ sum = 0
 with open('input.txt') as f:
     for line in f:
         #each astronomical unit, divide by 3, round down and subtract 2
-        line1 = math.floor(int(line)/3) - 2
+        line1 = math.floor(int(line)/3.0) - 2
         # add the calculated fuel required to the total sum
         sum = sum + line1
 
